e03334f345eed550cb63bf32736b3eec7394bc2d
fleet admiral <jujutsukaisen77700@gmail.com>
2025-10-01 08:47:28 +0530

    Delete backend/config/models directory

--- backend/config/models/Upload.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const uploadSchema = new mongoose.Schema({
-  file: String,
-  graphs: Array,
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Upload', uploadSchema);
